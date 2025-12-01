3b0fa6d6c0326c362c697087bee0433a9f5d52a1
Calvinan132 <trinhhoaian2303@gmail.com>
2025-12-01 20:54:55 +0700

    111

--- Portfolio-frontend/src/App.jsx
+++ Portfolio-frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./page/HomePage";
-import Portfolio from "./page/Portfolio";
 
 function App() {
   return (
